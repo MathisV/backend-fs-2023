bd4d38e8e5c846884d2bfe04e32499360da27d89
Mathis Verleene <mathis.verleene1@decathlon.com>
2023-05-06 22:52:53 +0200

    Add login and register

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -2,22 +2,93 @@ import { Router, Request, Response } from 'express';
 import jwt, { VerifyErrors } from 'jsonwebtoken';
 import { connectToDatabase } from '../middleware/db';
 import { JWT_SECRET, app } from '../main';
+import { sha256 } from 'js-sha256';
 
 const router = Router();
 
-router.post('/login', (req: Request, res: Response) => {
-    const username = req.body.username;
-    const password = req.body.password;
-  
+router.post('/login', async (req: Request, res: Response) => {
     // Vérifiez les identifiants de l'utilisateur ici (par exemple, recherchez l'utilisateur dans la base de données et vérifiez le mot de passe)
     // Pour l'instant, nous supposerons que les informations d'identification sont valides
   
     const user = {
-      username: username,
+      username: req.body.username,
+      password: sha256(req.body.password),
     };
-  
-    const accessToken = jwt.sign(user, JWT_SECRET, { expiresIn: '1h' });
-    res.json({ accessToken });
+
+    // Vérifier si l'utilisateur existe dans la base de données et si le mot de passe est correct
+    const connection = await connectToDatabase();
+    if (!connection) {
+      res.status(500).send("Error connecting to database");
+      return;
+    }
+    
+    const rows = await connection.query(
+      "SELECT * FROM users WHERE username = ? AND password = ?",
+      [user.username, user.password]
+    );
+    const obj_rows = Object.values(JSON.parse(JSON.stringify(rows[0])));
+    if (obj_rows.length === 0) {
+      res.status(404).send("User not found");
+      return;
+    } else if (obj_rows.length == 1) {
+      const element = JSON.parse(JSON.stringify(obj_rows));
+      console.log(element[0].username);
+
+      // Générer un jeton d'accès
+      const accessToken = jwt.sign(user, JWT_SECRET, { expiresIn: '1h' });
+      //res.json({ accessToken });
+
+      // Mettre à jour le bearer de l'utilisateur dans la base de données
+      const update = await connection.query(
+        "UPDATE users SET bearer = ? WHERE username = ?",
+        [accessToken, user.username]
+      );
+      connection.end();
+      if (update) {
+        console.log("Bearer updated");
+      }
+
+      // Token en cookie
+      res.cookie('token', accessToken, { httpOnly: true });
+      res.send("Login successful");
+    }
   });
+
+
+router.post('/register', async (req: Request, res: Response) => {
+  const user = {
+    username: req.body.username,
+    password: sha256(req.body.password),
+  };
+  const connection = await connectToDatabase();
+  if (!connection) {
+    res.status(500).send("Error connecting to database");
+    return;
+  }
+
+  // check id username already exists
+  const rows = await connection.query(
+    "SELECT * FROM users WHERE username = ?",
+    [user.username]
+  );
+  const obj_rows = Object.values(JSON.parse(JSON.stringify(rows[0])));
+  console.log(obj_rows[0]);
+  console.log(obj_rows.length);
+  if (obj_rows.length != 0) {
+    res.status(409).send("Username already exists");
+    return;
+  } else
+  {
+    const reg = await connection.query(
+      "INSERT INTO users (username, password, bearer) VALUES (?, ?, 'null')",
+      [user.username, user.password]
+    );
+    connection.end();
+    if (reg) {
+      res.status(201).send("User created");
+      return;
+    } 
+  }
+});
 
 export default router;
--- src/routes/stocks.ts
+++ src/routes/stocks.ts
@@ -1,35 +1,87 @@
-import { Router, Request, Response } from 'express';
-import { connectToDatabase } from '../middleware/db';
+import { Router, Request, Response } from "express";
+import { connectToDatabase } from "../middleware/db";
+import Binance from "node-binance-api";
+import axios, { AxiosHeaders } from "axios";
+
+const binance = new Binance().options({
+  APIKEY: process.env.BINANCE_API_KEY,
+  APISECRET: process.env.BINANCE_API_SECRET,
+});
+
+
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('List of all stocks');
+router.get("/", (req: Request, res: Response) => {
+  res.send("List of all stocks");
 });
 
-router.get('/:symbol', async (req: Request, res: Response) => {
+router.get("/:symbol", async (req: Request, res: Response) => {
   const symbol = req.params.symbol;
-  
+
   const connection = await connectToDatabase();
   if (!connection) {
-    res.status(500).send('Error connecting to database');
+    res.status(500).send("Error connecting to database");
     return;
   }
 
   try {
-    const rows = await connection.query('SELECT * FROM stocks WHERE symbol = ?', [symbol]);
+    const rows = await connection.query(
+      "SELECT * FROM stocks WHERE symbol = ?",
+      [symbol]
+    );
     connection.end();
+    const obj_rows = Object.values(JSON.parse(JSON.stringify(rows)));
+    if (obj_rows.length === 0) {
+      res.status(404).send("Stock not found");
+      return;
+    } else if (obj_rows.length == 2) {
+      const element = JSON.parse(JSON.stringify(obj_rows[0]));
+      let headers = {
+        "Content-Type":"multipart/form-data",
+        Accept:"application/json",
+      };
+      // Axios post request on https://api.flaticon.com/v3/app/authentication
+      const data = await axios.post(
+        "https://api.flaticon.com/v3/app/authentication",
+        {
+          apikey: process.env.FLATICON_API,
+        },
+        { headers: headers }
+      );
+      let token: String = data.data.data.token;
 
-    if (rows[0] === null) {
-      res.status(404).send('Stock not found');
-    } else {
-      res.json(rows[0]);
+      var headers_get = {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + token
+      };
+      // Use flaticon API to get icon of stock
+      const icon = await axios.get(
+        "https://api.flaticon.com/v3/search/icons/priority?q=" + element[0].symbol,
+        { headers: headers_get }
+      );
+      const icon_url = icon.data.data[0].images['512'];
+      binance.prices(symbol + "USDT", (error: any, ticker: any) => {
+        const result = {
+          info: element[0],
+          icon: icon_url,
+          ticker,
+        };
+        res.json(result);
+      });
     }
   } catch (error) {
     connection.end();
-    console.error('Error querying the database:', error);
-    res.status(500).send('Error querying the database');
+    console.error("Error querying the database:", error);
+    res.status(500).send("Error querying the database");
   }
+});
+
+router.get("/try/:symbol", async (req: Request, res: Response) => {
+  const symbol = req.params.symbol;
+  const test = binance.prices(symbol, (error: any, ticker: any) => {
+    res.json(ticker);
+  });
 });
 
 export default router;
