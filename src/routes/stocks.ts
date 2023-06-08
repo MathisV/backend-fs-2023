a4294c1d4ab41caea7b6717625d49d4cdc3ffb51
Mathis Verleene <mathis.verleene1@decathlon.com>
2023-06-08 15:12:59 +0200

    Fix stock/:symbol endpoint

--- src/routes/stocks.ts
+++ src/routes/stocks.ts
@@ -61,7 +61,7 @@ router.get("/:symbol", token.authenticateToken, async (req: Request, res: Respon
       res.status(404).json({message: "Stock not found", status: 404});
       logger(req, res, () => {});
       return;
-    } else if (obj_rows.length == 2) {
+    } else if (obj_rows.length == 1) {
       const element = JSON.parse(JSON.stringify(obj_rows[0]));
       let headers = {
         "Content-Type":"multipart/form-data",
@@ -76,16 +76,15 @@ router.get("/:symbol", token.authenticateToken, async (req: Request, res: Respon
         { headers: headers }
       );
       let token: String = data.data.data.token;
-
       var headers_get = {
         'Accept': 'application/json',
         'Authorization': 'Bearer ' + token
       };
       // Use flaticon API to get icon of stock
       const icon = await axios.get(
-        "https://api.flaticon.com/v3/search/icons/priority?q=" + element[0].symbol,
+        "https://api.flaticon.com/v3/search/icons/priority?q=" + symbol,
         { headers: headers_get }
-      );
+      )
       const icon_url = icon.data.data[0].images['512'];
       binance.prices(symbol + "USDT", (error: any, ticker: any) => {
         const result = {
